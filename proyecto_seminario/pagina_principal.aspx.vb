8c5f92400f552f7c73580fbaab9ddcccce62b796
Rubens <rubenscf2@gmail.com>
2015-11-10 01:59:57 -0600

    Sesion y menu

--- proyecto_seminario/pagina_principal.aspx.vb
+++ proyecto_seminario/pagina_principal.aspx.vb
@@ -1,18 +1,30 @@
 ﻿Imports Ext.Net
 Public Class pagina_principal
     Inherits System.Web.UI.Page
-
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         If Session.Count = 0 Then
             FormsAuthentication.RedirectToLoginPage()
-        Else
-            BtnSesionCnfg.Text = "Bienvenido " + Session("nombre")
+        End If
+        If Not Page.IsPostBack And Not Ext.Net.X.IsAjaxRequest Then
+            fMostrarValoresInicales()
         End If
     End Sub
+
+    Private Sub fMostrarValoresInicales()
+        BtnSesionCnfg.Text = "Bienvenido " + Session("nombre")
+        mostrarMenu()
+    End Sub
+    Protected Sub mostrarMenu()
+
+    End Sub
+#Region "Metodos Directos"
     <DirectMethod>
     Public Sub Salir()
         Session.Abandon()
-        FormsAuthentication.RedirectToLoginPage()
+        Session.Remove("nombre")
+        FormsAuthentication.SignOut()
+        Response.Redirect(Request.UrlReferrer.ToString())
     End Sub
 
+#End Region
 End Class
